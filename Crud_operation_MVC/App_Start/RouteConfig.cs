f3e2ded43a40e52c890dfb6212f303f1a15072aa
rupesh055202 <rupesh055202@gmail.com>
2025-07-02 16:53:16 +0530

    Add project files.

---
+++ Crud_operation_MVC/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Crud_operation_MVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "emps", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
